faf58f6c0c846428d68a2fa3f44fed32a926feb4
hasanfci@yahoo.com <hasanfci@yahoo.com>
2020-06-11 09:50:25 -0700

    Function call to another function in controller

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -4,6 +4,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Main extends CI_Controller {
 	public function index()
 	{
-		echo "test main";;
+		echo "test main <br>";
+		$this->test1();
+	}
+
+	public function test1(){
+		echo "This is test1 function";
 	}
 }
